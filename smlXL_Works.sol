c80d34376705f1206d706207a4e5902be237b71b
Nassim Dehouche <65496410+ndehouche@users.noreply.github.com>
2021-12-13 01:53:50 +0700

    Update smlXL_Works.sol

--- smlXL_Works.sol
+++ smlXL_Works.sol
@@ -1,13 +1,16 @@
 pragma solidity 0.8.9;
 //SPDX-License-Identifier: MIT
-import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-contract smlXL_Works is IERC721Receiver {
+//import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+
+contract smlXL_Works 
+//is IERC721Receiver 
+{
 uint depositId;
 event axieDeposited(
        //bytes32 indexed requestId,
         address indexed depositor,
-        address erc721contract, //may be unnecessary all over contract if only dealing with Axie
+        address erc721contract, //Unnecessary all over contract if only dealing with Axie
         uint256 tokenId
     );
 
@@ -21,14 +24,15 @@ IERC721 nftContract = IERC721(erc721contract);
 //Transfer the NFT from the user to the contract
 //assumes that a JavaScript call "await axieContract.approve(address(this), expectedTokenId)" has been 
 //previously made with the NFT owner's account. "msg.sender" in the external call below will be the contract's address. 
-//Though the Axie contract "was deployed before ERC721 was standardized", it implements the standard functions approve 
-//and safeTransferFrom. 
+//Though the Axie contract "was deployed before ERC721 was standardized", it implements the standard functions approve, 
+//transferFrom and safeTransferFrom. 
 //safeTransferFrom checks that msg.sender owns the NFT in question (we also check this offchain).
 //Also, it only works if the receiver is either an EOA or a smart contract that implements 
 //onERC721Received (implemented in the IERC721Receiver interface). Otherwise the call will revert.
-//We can also use the "unsafe" and simpler transferFrom. safeTransferFrom only exists to avoid 
+//Here, we use the "unsafe" and simpler transferFrom. safeTransferFrom only exists to avoid 
 //transfers to contracts that cannot handle being sent NFT. 
-nftContract.safeTransferFrom(msg.sender, address(this), tokenId);
+//nftContract.safeTransferFrom(msg.sender, address(this), tokenId);
+nftContract.transferFrom(msg.sender, address(this), tokenId);
 emit axieDeposited(msg.sender,  erc721contract, tokenId);
 }
 
@@ -52,14 +56,14 @@ IERC721 nftContract = IERC721(erc721contract);
 
 
 // @Required function to instanciate this contract as an IERC721 receiver
-    function onERC721Received(
-        address,
-        address,
-        uint256,
-        bytes memory
-    ) public virtual override returns (bytes4) {
-        return this.onERC721Received.selector;
-    }
+//    function onERC721Received(
+//        address,
+//        address,
+//        uint256,
+//        bytes calldata
+//    ) public virtual override returns (bytes4) {
+//        return this.onERC721Received.selector;
+//    }
 
 
 }
